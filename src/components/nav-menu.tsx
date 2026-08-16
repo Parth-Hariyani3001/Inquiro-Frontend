86237f2daf0aab06919fd2c47ea691ee9f6cedcc
Parth-Hariyani3001 <parthhariyani00@gmail.com>
2026-08-16 15:54:13 +0530

    updated logo and landing page navigation

--- src/components/nav-menu.tsx
+++ src/components/nav-menu.tsx
@@ -13,26 +13,26 @@ import { Link } from '@tanstack/react-router'
 export const NavMenu = (props: ComponentProps<typeof NavigationMenu>) => (
   <NavigationMenu {...props}>
     <NavigationMenuList className="data-[orientation=vertical]:-ms-2 data-[orientation=vertical]:flex-col data-[orientation=vertical]:items-start data-[orientation=vertical]:justify-start">
-      <NavigationMenuItem>
+      {/* <NavigationMenuItem>
         <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
           <Link to="/">Home</Link>
         </NavigationMenuLink>
-      </NavigationMenuItem>
-      <NavigationMenuItem>
+      </NavigationMenuItem> */}
+      {/* <NavigationMenuItem>
         <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
           <Link to="/">Blog</Link>
         </NavigationMenuLink>
-      </NavigationMenuItem>
-      <NavigationMenuItem>
+      </NavigationMenuItem> */}
+      {/* <NavigationMenuItem>
         <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
           <Link to="/">About</Link>
         </NavigationMenuLink>
-      </NavigationMenuItem>
-      <NavigationMenuItem>
+      </NavigationMenuItem> */}
+      {/* <NavigationMenuItem>
         <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
           <Link to="/">Contact Us</Link>
         </NavigationMenuLink>
-      </NavigationMenuItem>
+      </NavigationMenuItem> */}
     </NavigationMenuList>
   </NavigationMenu>
 )
